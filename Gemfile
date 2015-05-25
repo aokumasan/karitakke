17cb4f0cef6c0c1e7e63f49d0e8ffc1da9ae6729
Kouta ASAI <scene.4913@gmail.com>
2015-05-25 23:25:26 +0900

    devise-bootstrap-viewをインストールしました。

--- Gemfile
+++ Gemfile
@@ -40,9 +40,10 @@ group :development, :test do
   gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
+  gem 'spring', '~> 1.3.6'
 end
 
 gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootstrap-rails.git'
 gem 'less-rails'
 gem 'devise'
+gem 'devise-bootstrap-views', :git => 'git://github.com/hisea/devise-bootstrap-views.git'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def bootstrap_devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t('errors.messages.not_saved',
+    count: resource.errors.count,
+    resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div class="alert alert-danger alert-block">
+      <button type="button" class="close" data-dismiss="alert">x</button>
+      <h5>#{sentence}</h5>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
 end
